2de1298f3b2a6448099dece17fa55ea97cac320e
naveen17797 <kmnaveen101@gmail.com>
2016-04-04 09:48:12 +0530

    Added files via upload

--- generator.php
+++ generator.php
@@ -1,10 +1,12 @@
 <?php
+$arrr = $_GET["length1"];
 function rand_string( $length ) {
+	
 
     $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     return substr(str_shuffle($chars),0,$length);
 
 }
 
-echo rand_string(8);
+echo rand_string($arrr);
 ?>
--- page.php
+++ page.php
@@ -5,6 +5,7 @@
 <p align="center">hercules 8 digit password generator</p></p>
 </font>
 <form action="generator.php">
+<p align="center">length<input type="text" name="length1"></p>
 <p align="center"><input type="submit"> </p>
 </form>
 <html>
