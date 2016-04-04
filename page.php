5666b2adbf7a4ad07700e1a2fa0ecb4b4dcde15a
naveen17797 <kmnaveen101@gmail.com>
2016-04-04 09:17:08 +0530

    Added files via upload

---
+++ page.php
@@ -0,0 +1,10 @@
+<html>
+
+<font size="25">
+<p style="color:white; background-color:gray;">
+<p align="center">hercules 8 digit password generator</p></p>
+</font>
+<form action="generator.php">
+<p align="center"><input type="submit"> </p>
+</form>
+<html>
