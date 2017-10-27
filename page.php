72d705af7e4ff934f1d89a53b0eccec07cef321e
naveenking17 <33157707+naveenking17@users.noreply.github.com>
2017-10-27 20:27:46 +0530

    updated page files

--- page.php
+++ page.php
@@ -8,4 +8,24 @@
 <p align="center">length<input type="text" name="length1"></p>
 <p align="center"><input type="submit"> </p>
 </form>
+<html><html>
+
+<font size="25">
+<p style="color:white; background-color:gray;">
+<p align="center">hercules 8 digit password generator</p></p>
+</font>
+<form action="generator.php">
+<p align="center">length<input type="text" name="length1"></p>
+<p align="center"><input type="submit"> </p>
+</form>
+<html><html>
+
+<font size="25">
+<p style="color:white; background-color:gray;">
+<p align="center">hercules 8 digit password generator</p></p>
+</font>
+<form action="generator.php">
+<p align="center">length<input type="text" name="length1"></p>
+<p align="center"><input type="submit"> </p>
+</form>
 <html>
